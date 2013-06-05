a5a0698ce29dc77f3ce7997c8fe1aa5e35b9d08d
孙进辉 <s_jinhui@sina.com>
2013-06-05 20:20:57 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ ============================================================================
+ Name        : testc.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include<stdio.h>
+int main() {
+  printf("Hello World!\n");
+}
